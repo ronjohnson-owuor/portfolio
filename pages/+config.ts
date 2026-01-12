6df3e99ad74ed08c9c19534380959f679de435fa
ronjohnson_owuor <ronjohnsonowuor83@gmail.com>
2026-01-12 09:05:32 +0300

    added ssr deployment feature to be able to deploy on render

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'vike/types';
 import vikeReact from 'vike-react/config';
+import vikePhoton from 'vike-photon/config';
 
 export default {
 	title: 'Ron Johnson Owuor',
 	description:
 		'Hello there, my name is Ron Johnson Owuor kenyan software Engineer',
-	extends: [vikeReact],
+	extends: [vikeReact, vikePhoton],
 } satisfies Config;
